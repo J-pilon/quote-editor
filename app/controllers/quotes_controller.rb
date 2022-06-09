78b9223dcc44a312f4e03de588cb0f9c41abfc96
Josiah <josiah.pilon@gmail.com>
2022-06-09 15:06:22 -0700

    Updates to use turbo stream format

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -16,7 +16,10 @@ class QuotesController < ApplicationController
         @quote = Quote.new(quote_params)
 
         if @quote.save
-            redirect_to quotes_path, notice: "Quote was successfully created."
+            respond_to do |format|
+                format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
+                format.turbo_stream
+            end
         else
             render :new, status: :unprocessable_entity
         end
@@ -35,7 +38,12 @@ class QuotesController < ApplicationController
 
     def destroy
         @quote.destroy
-        redirect_to quotes_path, notice: "Quote was successfully destroyed."
+
+        respond_to do |format|
+            format.html { redirect_to quotes_path, notice: "Quote was successfully destroyed." }
+            format.turbo_stream
+        end
+
     end
 
     private
